efe850006f37192b50d91d7d4dda6182cb058600
chenshenhai <cshenhai@gmail.com>
2020-04-07 23:54:41 +0800

    feat: init wasm-pack

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
 mod core;
 extern crate regex;
 use crate::core::compile;
+use wasm_bindgen::prelude::*;
 
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
 pub fn parse(md: &str) -> String {
     let result = compile(md);
     return result;
